39bdd17c468a7aba8903487232906ba4630be977
the-luckyowl <75254350+the-luckyowl@users.noreply.github.com>
2021-03-31 13:18:00 +0200

    added userinfo page. for the user to change their information.

---
+++ accountpages/userInfo.php
@@ -0,0 +1,29 @@
+<?php require_once "../headerAndFooter/header.html"?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../headerAndFooter/includes/css/headerAndFooter.css">
+    <link rel="stylesheet" href="includes/css/accountMenu.css">
+    <title>user info</title>
+</head>
+
+<body>
+<div id="container">
+    <div id="accountManager">
+    <div class="item">account informatie</div>
+    <div class = "item">vrienden</div>
+    <div class="item">gekochten concerten</div>
+    </div>
+    <div id="tabsContainer">
+
+    </div>
+</div>
+<script src = "includes/userInfoJs.js"></script>
+<?php require_once "../headerAndFooter/footer.html"?>
+</body>
+
+</html>
