cb356ef947ff44a28545372d2a7685421920b95c
the-luckyowl <75254350+the-luckyowl@users.noreply.github.com>
2021-04-06 21:46:05 +0200

    added a search function, with javascript.

--- accountpages/includes/userInfoJs.js
+++ accountpages/includes/userInfoJs.js
@@ -1,15 +1,89 @@
+//wait till everything is loaded then go to init function.
 window.addEventListener("load",init);
-
-const menu = document.getElementById('accountManager');
-
+//create object array to search from
+let cats = [{name:'feniks', number: 28},
+    {name:'shifu', number:2},
+    {name:'leela', number:22},
+    {name:'raspberry', number:14},
+    {name:'leela', number:41},
+    {name:'marvel', number:41},
+    {name:'mazzel', number:41}
+]
+//get search input from html
+const searchBar = document.getElementById('searchBar');
+const concertList = document.getElementById('concertOverview');
+let value;
 function init(){
-menu.addEventListener('click',menuClickHandler)
+    //add eventlistener on searchbar.
+    searchBar.addEventListener('keyup',(event)=>{
+        //get the input value
+        value = event.target.value;
+        //check value and if there is a value with out spaces.
+        if (value && value.trim().length>0){
+            //take out spaces and set search query to lowercase
+            value = value.trim().toLowerCase();
+            //filter through array to find matches.
+            setList(cats.filter(number =>{
+                //return results that include the search query
+                return number.name.includes(value);
+            }).sort((resultA,resultB) => {
+               return sortResults(resultB.name, value) - sortResults(resultA.name, value);
+            }));
+
+        }else{
+            clearList();
+        }
+    })
 }
-function menuClickHandler(e){
-    let clickedItem = e.target
-    let items = document.getElementsByClassName('item');
-    if ( clickedItem.classList.contains ){
-        return;
+function sortResults(value, searchterm){
+    //closest value returns highest
+    if (value === searchterm){
+        return 2;
     }
-    clickedItem.classList.toggle( 'active');
+    //then if it starts with the searchterm
+    else if (value.startsWith(searchterm)){
+        return 1;
+    }
+    //then if the searchterm is anywhere in the value
+    else{
+        return 0;
+    }
 }
+function setList(group){
+clearList();
+    for (const cat of group){
+    let listItem = document.createElement('li');
+    listItem.classList.add();
+    listItem.innerText = (cat.name);
+    concertList.appendChild(listItem);
+    }
+    if (group.length === 0){
+        noResults();
+    }
+}
+function noResults(){
+    let listItem = document.createElement('li');
+    listItem.classList.add();
+    listItem.innerText = ('No result');
+    concertList.appendChild(listItem);
+}
+function clearList(){
+    while (concertList.firstChild){
+        concertList.removeChild(concertList.firstChild);
+    }
+}
+// const menu = document.getElementById('accountManager');
+// const menuItems = document.getElementsByClassName('item');
+//use trim to remove spaces inside search
+// function menuClickHandler(e){
+//     let clickedItem = e.target;
+//         if (clickedItem.classList.contains('item')){
+//             console.log('hello');
+//             clickedItem.setAttribute('class','item active' );
+//         }
+//     else if (clickedItem.classList.contains('item') === true){
+//         console.log('no longer active');
+//         clickedItem.classList.remove('class');
+//     }
+//
+// }
--- accountpages/userInfo.php
+++ accountpages/userInfo.php
@@ -3,27 +3,36 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport"  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../headerAndFooter/includes/css/headerAndFooter.css">
     <link rel="stylesheet" href="includes/css/accountMenu.css">
-    <title>user info</title>
+    <link rel="stylesheet" href="../headerAndFooter/includes/css/mobileLayout.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" >
+    <title>gebruiker</title>
 </head>
 
 <body>
-<div id="container">
-    <div id="accountManager">
+<main id="container">
+    <nav id="accountManager">
     <div class="item">account informatie</div>
     <div class = "item">vrienden</div>
     <div class="item">gekochten concerten</div>
-    </div>
+    </nav>
     <div id="tabsContainer">
 
     </div>
-</div>
+    <div id="concertSearchBar">
+        <label for="searchBar"></label><input type="text" name="searchBar" id="searchBar" placeholder="search">
+    </div>
+    <ul id="concertOverview">
+
+    </ul>
+</main>
 <script src = "includes/userInfoJs.js"></script>
 <?php require_once "../headerAndFooter/footer.html"?>
+<script src = "../headerAndFooter/includes/toggleMenu.js"></script>
+
 </body>
 
 </html>
