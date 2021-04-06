aaa3b4dca5e94dfd544c9fc858f82f19f40dde04
the-luckyowl <75254350+the-luckyowl@users.noreply.github.com>
2021-04-06 21:58:24 +0200

    no changes made.

---
+++ frontpage/homepage.php
@@ -0,0 +1,34 @@
+<?php require_once "../headerAndFooter/header.html"?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type= "text/css" href="../headerAndFooter/includes/css/headerAndFooter.css">
+    <link rel="stylesheet" type= "text/css" href="includes/homepageStyle.css">
+    <link rel="stylesheet" href="../headerAndFooter/includes/css/mobileLayout.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <title>frontpage</title>
+</head>
+<body>
+<div id = "container">
+
+
+    <main id="introContainer">
+        <section id="intro">
+            <h1>
+                grounds intro
+            </h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.
+            </p>
+        </section>
+    </main>
+</div>
+
+<?php require_once "../headerAndFooter/footer.html"?>
+
+</body>
+<script src = "../headerAndFooter/includes/toggleMenu.js"></script>
+</html>
