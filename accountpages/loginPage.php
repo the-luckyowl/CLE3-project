ed60be2b98a53868c05e480bd74ba4c00e593ba1
the-luckyowl <75254350+the-luckyowl@users.noreply.github.com>
2021-04-06 21:57:22 +0200

    no changes.

--- accountpages/loginPage.php
+++ accountpages/loginPage.php
@@ -10,23 +10,27 @@ require_once "../headerAndFooter/header.html";
 <head>
     <meta charset="UTF-8">
     <title>login</title>
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../headerAndFooter/includes/css/headerAndFooter.css">
     <link rel="stylesheet" href="includes/css/loginCss.css">
+    <link rel="stylesheet" href="../headerAndFooter/includes/css/mobileLayout.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
 </head>
 <body>
-<header>
 
-</header>
-<div id="formContainer">
-<form>
+<main id="formContainer">
+<form action="">
     <input type="text" name="username" class="form-control" placeholder="username">
     <br>
     <input type="password" name="password" class="form-control" placeholder="password">
     <br>
     <input type="submit" class="btn-primary" value="Login">
 </form>
-</div>
-</body>
+</main>
 <?php require_once "../headerAndFooter/footer.html"; ?>
+<script src = "../headerAndFooter/includes/toggleMenu.js"></script>
+
+</body>
+
 </html>
